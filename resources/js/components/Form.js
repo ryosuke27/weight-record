5186e2fec1cad358f562be1e903e1018c8e7c9ac
{ryosuke27} <opu.7455@gmail.com>
2022-04-21 12:52:01 +0900

    setWeight

--- resources/js/components/Form.js
+++ resources/js/components/Form.js
@@ -1,9 +1,13 @@
+import { useState } from "react";
+
 function Form() {
+    const [weight, setWeight] = useState("");
     return (
         <form>
             <div className="card-body">
-                <input type="number" name="weight" />
+                <input type="text" name="weight" placeholder="your weight" onChange={e => setWeight(e.target.value)}/>
                 <button type="submit">store</button>
+                { weight }
             </div>
         </form>
     );
